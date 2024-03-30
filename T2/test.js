a89a15c412a60b65eeca109d2b7c85bab16707b8
chlience <chlience@outlook.com>
2024-03-30 16:55:38 +0800

    test(T2 测试): 在 test.js 中新增一个测试用例

--- T2/test.js
+++ T2/test.js
@@ -11,4 +11,6 @@ import { mancalaResult } from "./load.js";
 assert.strictEqual(mancalaResult(1,[11,12],2),30001);
 assert.strictEqual(mancalaResult(1,[14],1),20001);
 
+assert.strictEqual(mancalaResult(1, [12, 21, 16, 22, 11, 26, 15, 24, 15, 21, 16, 23, 14, 25, 16, 23, 13, 25, 15, 26, 14, 11, 23, 12, 21, 13, 24, 13, 22, 15, 23, 16, 21, 14, 23, 16, 12, 21, 15, 22, 11, 26, 14, 24, 11, 25], 46), 15011);
+
 console.log("🎉 You have passed all the tests provided.");
